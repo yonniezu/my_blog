fa4b0208abe5fb420a1e2cae8627e8004d1338fd
will <will@it-c-4.local.lan>
2018-04-16 09:36:58 +0800

    your commit message

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
+	if request.method == 'POST' : 
+		comment = request.POST.get('conmment')
+		user = request.POST.get("username")
+
+		print(comment * 10)
+		return render(request, 'blog/home_page.html', {'comment': comment, 'username': user})
+
 	return render(request, 'blog/home_page.html')
+	
